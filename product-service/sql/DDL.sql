96ab857b408d66fcaf94109adcd681a83bed0e32
Maksim Trigolos <Maksim_Trigolos@epam.com>
2020-11-04 19:13:32 +0400

    feat: Add sql scripts for creating tables and fill one

---
+++ product-service/sql/DDL.sql
@@ -0,0 +1,16 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE products
+(
+    id          UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    title       TEXT NOT NULL,
+    description TEXT,
+    price       INTEGER
+);
+
+CREATE TABLE stocks
+(
+    product_id UUID,
+    count      INTEGER,
+    FOREIGN KEY ("product_id") REFERENCES "products" ("id")
+);
